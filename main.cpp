6c544d79bc26d62bf12e9fe8b701c1b64ac54d59
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-06-07 21:32:45 +0200

    table check

--- main.cpp
+++ main.cpp
@@ -8,12 +8,23 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+    //ładowanie bazy danych
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("localhost");
     db.setDatabaseName("uml_qt");
     db.setUserName("root");
     db.setPassword("");
     bool ok = db.open();
+
+    QSqlQuery query;
+    query.exec("SELECT * FROM konta");
+    if (!query.next())
+    {
+        query.exec("CREATE TABLE `uml_qt`.`konta` ( `login` VARCHAR(40) NOT NULL , `haslo` VARCHAR(40) NOT NULL , `typ` ENUM('Organizator','Obsluga','Doradca','Administrator') NOT NULL , PRIMARY KEY (`login`))");
+    }
+    //query.exec("create table xxx (a int)"); <----- działa
+
+
 
 
     return aplikacja.exec();
