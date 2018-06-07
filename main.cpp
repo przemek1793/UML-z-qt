9470d5e066ab4dc5b0d52fb562e85d8da4a109d1
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-06-07 18:04:20 +0200

    sql

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtSql>
 
 int main(int argc, char *argv[])
 {
     QApplication aplikacja(argc, argv);
     MainWindow w;
     w.show();
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("uml_qt");
+    db.setUserName("root");
+    db.setPassword("");
+    bool ok = db.open();
+
 
     return aplikacja.exec();
 }
