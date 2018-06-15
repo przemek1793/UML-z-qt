ae11878604b632d3b8353f4008a52b79f9df052b
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-06-15 15:50:34 +0200

    dobrze wypełnia ui zmiany danych bez doradców

--- zmiendaneprzyjecia.cpp
+++ zmiendaneprzyjecia.cpp
@@ -4,6 +4,7 @@
 #include <QtSql>
 
 extern QStackedWidget* stack;
+extern QString loginZalogowany;
 
 ZmienDanePrzyjecia::ZmienDanePrzyjecia(int indeks, const QDate* data1, QWidget *parent) :
     QDialog(parent),
@@ -12,6 +13,20 @@ ZmienDanePrzyjecia::ZmienDanePrzyjecia(int indeks, const QDate* data1, QWidget *
     date(data1)
 {
     ui->setupUi(this);
+    ui->Data->setText("Przyjęcie odbywające się "+date->toString());
+    QSqlQuery query;
+    query.exec("SELECT nazwa, zatrudniona_obsluga, zatrudnieni_doradcy FROM przyjecia where organizator='"+loginZalogowany+"' and data_wydarzenia=STR_TO_DATE('"+date->toString("dd-MM-yyyy")+"',\"%d-%m-%Y\")");
+    query.first();
+    ui->Nazwa->setText(query.value(0).toString()); //dobrze
+    QString obsluga=query.value(1).toString();
+    int ileObslugi=0;
+    if (obsluga!="")
+    {
+        QStringList Listaobslugi = obsluga.split(',');
+        ileObslugi=Listaobslugi.size()-1;
+    }
+    ui->obsluga->setText("Zatrudniono "+QString::number(ileObslugi)+" pracowników obsługi");
+    QStringList doradcy = query.value(2).toString().split(',');
 }
 
 ZmienDanePrzyjecia::~ZmienDanePrzyjecia()
