e7b4c63497e5f2a2478b1f6eebc85fee7b9e0a0f
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-06-16 14:54:52 +0200

    zapełnianie listy użytkoników

--- listauzytkownikow.cpp
+++ listauzytkownikow.cpp
@@ -1,15 +1,25 @@
 #include "listauzytkownikow.h"
 #include "ui_listauzytkownikow.h"
 #include <QStackedWidget>
+#include <QtSql>
 
 extern QStackedWidget* stack;
 extern int administrator;
+QString Obecny_login;
 
 ListaUzytkownikow::ListaUzytkownikow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ListaUzytkownikow)
 {
     ui->setupUi(this);
+
+    QSqlQuery query;
+    query.exec("SELECT * FROM `konta`");
+    while (query.next())
+    {
+        QString Obecny="Konto typu "+query.value(2).toString()+" o loginie "+query.value(0).toString();
+        ui->Lista->addItem(Obecny);
+    }
 }
 
 ListaUzytkownikow::~ListaUzytkownikow()
